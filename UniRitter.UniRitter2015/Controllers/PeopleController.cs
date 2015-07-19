3475d797107f607b21190eefdb8bfa30489f2bc2
rodrigodellinghausen <dellinghausen@gmail.com>
2015-07-18 21:04:51 -0300

    Corrige o construtor dos controllers.

--- UniRitter.UniRitter2015/Controllers/PeopleController.cs
+++ UniRitter.UniRitter2015/Controllers/PeopleController.cs
@@ -1,9 +1,12 @@
 ﻿using UniRitter.UniRitter2015.Models;
+using UniRitter.UniRitter2015.Services;
 
 namespace UniRitter.UniRitter2015.Controllers
 {
     public class PeopleController : BaseController<PersonModel>
     {
-        
+        public PeopleController(IRepository<PersonModel> repo) : base(repo)
+        {
+        }
     }
 }
--- UniRitter.UniRitter2015/Controllers/PostsController.cs
+++ UniRitter.UniRitter2015/Controllers/PostsController.cs
@@ -1,8 +1,12 @@
 ﻿using UniRitter.UniRitter2015.Models;
+using UniRitter.UniRitter2015.Services;
 
 namespace UniRitter.UniRitter2015.Controllers
 {
     public class PostsController : BaseController<PostModel>
     {
+        public PostsController(IRepository<PostModel> repo) : base(repo)
+        {
+        }
     }
 }
